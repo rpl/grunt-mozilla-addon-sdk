fb38a08bd8040e60c2d144292f1c7cf14ccbe2dc
olsh <bloodunit@gmail.com>
2013-10-03 10:52:23 +0400

    Fixed tests and script on windows

--- scripts/cfx.bat
+++ scripts/cfx.bat
@@ -2,5 +2,17 @@ cd %1
 call bin\activate
 cd %2
 set COMMAND=%3
-shift; shift; shift;
-cfx %COMMAND% %*
+
+SHIFT 
+SHIFT 
+SHIFT
+
+SET args=
+:LOOP
+if !%1!==!! goto :END
+SET args=%args% %1
+SHIFT
+goto LOOP
+:END
+
+cfx %COMMAND% %args%
